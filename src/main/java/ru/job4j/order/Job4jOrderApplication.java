aecfbfd3595538cfe03619cd775f451a5da044b5
Mouzzli <cokes03@yandex.ru>
2023-05-20 13:11:34 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/order/Job4jOrderApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/job4j/order/Job4jOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
